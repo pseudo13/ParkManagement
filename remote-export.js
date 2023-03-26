0fdbc74f7661b7ad70b7666de89e2aa8230c39eb
pseudo13 <86127281+pseudo13@users.noreply.github.com>
2023-03-26 15:34:27 +0200

    Add files via upload

---
+++ remote-export.js
@@ -0,0 +1,30 @@
+/**
+//SAMPLE URL: https://someserver.com/remoteComponentA.js
+
+//REFERENCE: https://github.com/angular-extensions/elements
+
+SAMPLE USAGE
+<DynamiComponent [url]="'https://someserver.com/remoteComponentA.js'"></DynamicComponent>
+*/
+
+function remoteComponentA() {
+    let template = `<div>
+						<h1>{{titleText}}</h1>
+						<button (click)="buttonClick($event, 'John')">
+							{{buttonLabel}}
+						</button>
+					</div>`;
+    let controller = function () {
+        this.titleText = "Title Text";
+        this.buttonLabel = "I am a Button";
+        this.buttonClick = function (e, name) {
+            alert(name);
+        }
+    }
+    return {
+        template,
+        controller
+    }
+}
+
+export remoteComponentA;
